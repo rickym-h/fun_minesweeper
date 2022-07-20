f718e64ad4916a0c4b0899671324922461d95b7f
rickym-h <richardhull11@gmail.com>
2022-07-20 15:15:50 +0100

    updated newboard button to regenerate a grid

--- script.js
+++ script.js
@@ -21,22 +21,38 @@ function newBoard(width, height, numOfMines) {
     // delete old nodes
     fieldDOM.innerHTML = '';
 
+    // todo style #field width and height based on the ratio of width/height
+
     //set styling taking into account the new nodes
     fieldDOM.style.gridTemplateRows = "repeat("+height.toString()+", 1fr)";
-    console.log("repeat("+height.toString()+", 1fr)");
     fieldDOM.style.gridTemplateColumns = "repeat("+width.toString()+", 1fr)";
-    console.log("repeat("+width.toString()+", 1fr)");
 
     for (let h = 0; h < height; h++) {
         for (let w = 0; w < width; w++) {
             let tile = document.createElement("div");
-
             tile.classList.toggle("tile")
-
             fieldDOM.appendChild(tile);
-
         }
     }
 }
 
 newBoard(9,9,1);
+
+
+document.getElementById("new-game").addEventListener("click", function() {
+    let size = document.getElementById("size").value
+    switch (size) {
+        case "s":
+            newBoard(9,9,1);
+            break;
+        case "m":
+            newBoard(16,16,40);
+            break;
+        case "l":
+            newBoard(32,16,99);
+            break;
+        default:
+            console.log("INVALID SIZE ENTERED: " + size)
+            return;
+    }
+})
